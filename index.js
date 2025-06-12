9e2e5b2dff57256fea7a74c80737c31ae39c8a9c
Jay Amko <22206489+Jay13Joseph@users.noreply.github.com>
2025-06-11 23:02:55 -0600

    Add function to get user input and compare the two choices as numbers.

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
+// const choices = { rock: 0, paper: 1, scissors: 2};
 function getComputerChoice() {
-  return Math.floor(Math.random() * 3)
+  return Math.floor(Math.random() * 3);
 }
 
-console.log(getComputerChoice())
+function getHumanChoice() {
+  let humanChoice = prompt('Please enter either "0 for rock," "1 for paper," or "2 for scissors"')
+   console.log(`You entered: ${humanChoice}`);
+  return Number(humanChoice);
+}
+
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+console.log(`Your opponent entered: ${computerSelection}`)
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice){
+    console.log("It's a tie!");
+  } else if (( computerChoice ===  0 && humanChoice === 1) || 
+           (computerChoice === 1 && humanChoice === 2) || 
+           (computerChoice === 2 &&  humanChoice === 0)) {
+    console.log("You win!");
+  } else {
+    console.log("You lose!");
+  }
+}
+
+
+
+playRound(humanSelection, computerSelection);
