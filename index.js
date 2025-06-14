9c5cea701e80e5a322d6cc2498c9fd3a32732a12
Jay Amko <22206489+Jay13Joseph@users.noreply.github.com>
2025-06-13 20:12:42 -0600

    Add function to increment user or computer score after a round. Add string announcing score.

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const choiceNames = ["rock", "paper", "scissors"];
 
+humanScore = 0
+computerScore = 0
+
 function getComputerChoice() {
   return choiceNames[Math.floor(Math.random() * 3)];
 }
 
 function getHumanChoice() {
   let humanChoice = prompt('Please enter either "rock," "paper," or "scissors"')
-   console.log(`You entered: ${humanChoice}`);
   return (humanChoice);
 }
 
 const humanSelection = getHumanChoice().toLowerCase().trim();
 const computerSelection = getComputerChoice();
-console.log(`Your opponent entered: ${computerSelection}`)
+
+
 
 function capitalizeFirst(str) {
   if (!str) return "";
@@ -20,31 +23,36 @@ function capitalizeFirst(str) {
 }
 
 function playRound(humanChoice, computerChoice) {
+   console.log(`You entered: ${humanChoice}`);
+   console.log(`Your opponent entered: ${computerSelection}`)
+
   if (humanChoice === computerChoice){
     console.log("It's a tie!");
   } else if (( computerChoice ===  "rock" && humanChoice === "paper") || 
            (computerChoice === "paper" && humanChoice === "scissors") || 
            (computerChoice === "scissors" &&  humanChoice === "rock")) {
     console.log(`You win! ${capitalizeFirst(humanChoice)} beats ${computerChoice}!`);
+    humanScore += 1;
   } else {
     console.log(`You lose! ${capitalizeFirst(computerChoice)} beats ${humanChoice}!`);
+    computerScore += 1;
   }
+  console.log(`The score is You: ${humanScore}, Ivan: ${computerScore}`)
 }
 
 function playGame() {
   console.log("Welcome to Rock, Paper, Scissors!");
   console.log("You will be playing against a computer opponent. His name is Ivan!");
   playRound(humanSelection, computerSelection);
-  playRound(humanSelection, computerSelection);
-  playRound(humanSelection, computerSelection);
-  playRound(humanSelection, computerSelection);
-  playRound(humanSelection, computerSelection);
+  // playRound(humanSelection, computerSelection);
+  // playRound(humanSelection, computerSelection);
+  // playRound(humanSelection, computerSelection);
+  // playRound(humanSelection, computerSelection);
 }
 
 playGame()
 
 // To-do:
-// Add a function to validate the input
 // Play 5 rounds
 // Add a function to keep track of score
 // Add a function to display final score      
