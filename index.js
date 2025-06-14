7a48975875e2f38684d6a10cce124122c73600a8
Jay Amko <22206489+Jay13Joseph@users.noreply.github.com>
2025-06-13 19:54:41 -0600

    Add string to announce winner. Add function to capitalize first letter of winning object.

--- index.js
+++ index.js
@@ -14,19 +14,34 @@ const humanSelection = getHumanChoice().toLowerCase().trim();
 const computerSelection = getComputerChoice();
 console.log(`Your opponent entered: ${computerSelection}`)
 
+function capitalizeFirst(str) {
+  if (!str) return "";
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === computerChoice){
     console.log("It's a tie!");
   } else if (( computerChoice ===  "rock" && humanChoice === "paper") || 
            (computerChoice === "paper" && humanChoice === "scissors") || 
            (computerChoice === "scissors" &&  humanChoice === "rock")) {
-    console.log("You win!");
+    console.log(`You win! ${capitalizeFirst(humanChoice)} beats ${computerChoice}!`);
   } else {
-    console.log("You lose!");
+    console.log(`You lose! ${capitalizeFirst(computerChoice)} beats ${humanChoice}!`);
   }
 }
 
-playRound(humanSelection, computerSelection);
+function playGame() {
+  console.log("Welcome to Rock, Paper, Scissors!");
+  console.log("You will be playing against a computer opponent. His name is Ivan!");
+  playRound(humanSelection, computerSelection);
+  playRound(humanSelection, computerSelection);
+  playRound(humanSelection, computerSelection);
+  playRound(humanSelection, computerSelection);
+  playRound(humanSelection, computerSelection);
+}
+
+playGame()
 
 // To-do:
 // Add a function to validate the input
